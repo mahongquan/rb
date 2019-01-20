612fe2c9d488a79bc7e267c38153810c1c4470c4
Your Name <you@example.com>
2019-01-20 12:36:17 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+yarn add @babel/preset-env @babel/plugin-syntax-jsx 
+@babel/plugin-transform-react-jsx @babel/plugin-transform-react-display-name @babel/plugin-proposal-class-properties @babel/plugin-transform-react-jsx-self @babel/plugin-transform-react-jsx-source  @babel/plugin-transform-flow-strip-types @babel/plugin-proposal-export-default-from @babel/plugin-proposal-export-namespace-from @babel/plugin-proposal-logical-assignment-operators @babel/plugin-proposal-optional-chaining  @babel/plugin-proposal-pipeline-operator @babel/plugin-proposal-nullish-coalescing-operator @babel/plugin-proposal-do-expressions
+
+
